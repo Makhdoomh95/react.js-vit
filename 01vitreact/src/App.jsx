ba239b9266352184a64ff9c93a33875f3f14d0a1
makhdoom hussain <makhdoomh95@gmail.com>
2024-07-11 10:42:56 -1200

    react.js vit project

---
+++ 01vitreact/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import Chai from "./chai"
+
+function App() {
+
+
+  return (
+    <>
+<Chai/>  
+<h1>kr har medan fateh </h1>
+<p>1st expression is the last express</p>
+</>
+  )
+  
+}
+
+export default App
---
+++ 01vitreact/src/chai.jsx
@@ -0,0 +1,9 @@
+function Chai(){
+
+    return(
+
+        <h1> learn and earn </h1>
+    )
+}
+
+export default Chai
